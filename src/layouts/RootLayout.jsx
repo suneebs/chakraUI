0535b2cc882a6b41c60ec44c82190cb7ed6a6c04
suneeb <ssuneebvishnu@gmail.com>
2024-02-19 01:39:12 +0530

    responsive styles in gridItems

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,11 +4,19 @@ import { Grid, GridItem } from "@chakra-ui/react"
 
 export default function RootLayout() {
   return (
-    <Grid templateColumns={'repeat(6, 1fr)'} bg={'grey.50'}>
-      <GridItem as="aside" colSpan={'1'} bg={'purple.400'} minHeight={'100vh'} p='30px'>
+    <Grid templateColumns="repeat(6, 1fr)" bg={'grey.50'}>
+      <GridItem 
+      as="aside" 
+      colSpan={{ base:6, lg:2, xl:1 }} 
+      bg={'purple.400'} 
+      minHeight={{lg:'100vh'}} 
+      p={{base:'20px', lg:'30px'}}>
       <span>sidebar</span>
       </GridItem>
-      <GridItem as='main' colSpan={'5'} p='40px'>
+      <GridItem 
+      as='main' 
+      colSpan={{ base:6, lg:4, xl:5 }}
+      p='40px'>
       <Navbar></Navbar>
       <Outlet />
       </GridItem>
