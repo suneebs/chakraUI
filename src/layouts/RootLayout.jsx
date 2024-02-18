c46bd5cbe88b483b2809b0b031372a48d531114e
suneeb <ssuneebvishnu@gmail.com>
2024-02-19 01:08:14 +0530

    learned Grid component

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 export default function RootLayout() {
   return (
-    <div>
+    <Grid templateColumns={'repeat(6, 1fr)'} bg={'grey.50'}>
+      <GridItem as="aside" colSpan={'1'} bg={'purple.400'} minHeight={'100vh'} p='30px'>
+      <span>sidebar</span>
+      </GridItem>
+      <GridItem as='main' colSpan={'5'} p='40px'>
       <Navbar></Navbar>
       <Outlet />
-    </div>
+      </GridItem>
+    </Grid>
   )
 }
