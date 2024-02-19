d7c5bedf13a82381ea253e47a34efd9dec2e1666
suneeb <ssuneebvishnu@gmail.com>
2024-02-19 13:11:20 +0530

    customizing the theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react' 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react' 
+
+// Extend the theme
+const colors={
+  brand:{
+    900:'#024fc9',
+    800:'#146af5',
+    700:'#2977f2',
+    600:'#337df2',
+    500:'#4287f5'
+  }
+}
+
+const fonts={
+  body:'Tahoma',
+  heading:'Courie New'
+}
+
+const theme = extendTheme({colors, fonts})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
